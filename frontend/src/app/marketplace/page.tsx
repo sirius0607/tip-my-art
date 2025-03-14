f6002373bb4d0727de74dc50ebe2f30953fd8fbc
sirius <s_alimi@hotmail.com>
2025-03-15 00:37:52 +0100

    feat: cleaning marketplace page

--- frontend/src/app/marketplace/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Marketplace } from "@/components/marketplace/Marketplace";
-
-export default function MarketplacePage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <Marketplace />
-    </div>
-  );
-} 
